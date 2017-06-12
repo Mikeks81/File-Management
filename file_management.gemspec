f29dd29ee5eb2b440dbc17aceb1748ea9c4bb9dd
Michael <mikeks81@gmail.com>
2017-06-11 22:57:24 -0400

    abstracted File manager to it's own class

--- file_management.gemspec
+++ file_management.gemspec
@@ -10,8 +10,8 @@ Gem::Specification.new do |spec|
   spec.email         = ["mikeks81@gmail.com"]
 
   spec.summary       = %q{File Management library for specific paths}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.description   = %q{Simple file management program to allow CRUD like functions to a single directory. }
+  spec.homepage      = "https://github.com/Mikeks81/File-Management"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
@@ -33,5 +33,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.15"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "rspec", "~> 3.0"
-  s.add_dependency "filesize", "~> 0.1.1"
+  spec.add_dependency "filesize", "~> 0.1.1"
 end
---
+++ lib/file_management/file_manager.rb
@@ -0,0 +1,79 @@
+require 'fileutils'
+require 'filesize'
+
+class FileManager
+  attr_accessor :path
+  attr_reader :excludes
+  def initialize(options = {})
+    @path = options[:path] || './'
+    @excludes = options[:excludes] || ''
+    resolve_path(@path)
+    options.each do |k,v|
+      if k != :path && k != :excludes
+        instance_variable_set('@' + k.to_s, v)
+        add_accessor(k)
+      end
+    end
+  end
+
+  def excludes=(value)
+    if value.nil? || value.class == Integer
+      raise FileManagementErrors.new('Invalid value for @excludes')
+    else
+      @excludes = value
+    end
+  end
+
+  def pwd(file)
+    "#{path}/#{file}"
+  end
+
+  def format_excludes
+    return excludes.split(' ') if excludes.class == String
+    excludes
+  end
+
+  def all_files(all = false)
+    file_list = Dir.entries(path).select { |f| File.file?("#{path}/#{f}") }
+    file_list - (%w(. .. .DS_Store file_management.rb) + format_excludes)
+  end
+
+  def file_exists?(file)
+    all_files.include?(file)
+  end
+
+  def stats(file, request)
+    File.stat(pwd(file)).send(request)
+  end
+
+  def file_size(file)
+    puts 'File does not exist' unless file_exists?(file)
+    Filesize.new(file, Filesize::SI).pretty
+  end
+
+  def make_file(file)
+    return false if file_exists?(file)
+    FileUtils.touch(pwd(file))
+  end
+
+  def rename(file, new_name)
+    return false unless file_exists?(File.split(new_name).last)
+    File.rename(pwd(file), pwd(new_name))
+  end
+
+  def delete_file(file)
+    return false unless file_exists?(file)
+    FileUtils.rm(pwd(file))
+  end
+
+  private
+
+  def add_accessor(name)
+    self.class.send(:attr_accessor, name)
+  end
+
+  def resolve_path(path)
+    FileUtils.mkdir_p(path.split)
+    raise FileManagementErrors.new('Directory could not be created') unless File.directory?(path)
+  end
+end
